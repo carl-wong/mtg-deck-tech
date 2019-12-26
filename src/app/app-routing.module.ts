ae9c51688da058f0bdec185a0818135af0676237
Carl Wong <carl@winsoft.ca>
2019-12-26 01:56:19 -0500

    Added scryfall oracle JSON

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+	// { path: '', redirectTo: '/main', pathMatch: 'full' },
+	// { path: 'add-ons', component: SectionAddOnsComponent, canActivate: [AuthGuard] },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule]
 })
 export class AppRoutingModule { }
