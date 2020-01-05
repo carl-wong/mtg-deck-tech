6972356ca831cfdb8ec95e86cdbcd3ca89cb331d
Carl Wong <carl@winsoft.ca>
2020-01-05 14:01:08 -0500

    Update environment.prod.ts
    For working API deployed to decktech-api.narl.life

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   defaultDecklist: "1 Plains\n1 Island\n1 Swamp\n1 Mountain\n1 Forest\n1 Wastes",
-  oracleApi: 'http://localhost:3000',
-  localApi: 'http://localhost:3001',
+  oracleApi: 'http://decktech-api.narl.life',
+  localApi: 'http://decktech-api.narl.life',
 };
