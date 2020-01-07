2262b23bd6f3758b18aa2d733416c52e375cd637
Carl Wong <carl@winsoft.ca>
2020-01-07 17:01:13 -0500

    Version 0.0.3

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ export const environment = {
   defaultDecklist: '1 Plains\n1 Island\n1 Swamp\n1 Mountain\n1 Forest\n1 Wastes',
   oracleApi: 'https://decktech-api.narl.life',
   localApi: 'https://decktech-api.narl.life',
-  timestamp: 'Updated: 2020-01-07 @ 16:38:56',
-  version: '0.0.1',
+  timestamp: 'Updated: 2020-01-07 @ 17:00:37',
+  version: 'v0.0.3',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,6 @@ export const environment = {
   defaultDecklist: '1 Alesha, Who Smiles at Death\n1 Altar of Dementia\n1 Animate Dead',
   oracleApi: 'http://localhost:3000',
   localApi: 'http://localhost:3000',
-  timestamp: 'Updated: 2020-01-07 @ 16:38:56',
+  timestamp: 'Updated: 2020-01-07 @ 17:00:37',
   version: 'LOCAL',
 };
