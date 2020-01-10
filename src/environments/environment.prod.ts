6c51fc0fff7d3ca375ae15541bfd3826512ff745
Carl Wong <carl@winsoft.ca>
2020-01-10 18:00:50 -0500

    New version with add tag tooltip removed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ export const environment = {
   defaultDecklist: '1 Plains\n1 Island\n1 Swamp\n1 Mountain\n1 Forest\n1 Wastes',
   oracleApi: 'https://decktech-api.narl.life',
   localApi: 'https://decktech-api.narl.life',
-  timestamp: 'Updated: 2020-01-10 @ 17:12:27',
-  version: 'v0.0.16',
+  timestamp: 'Updated: 2020-01-10 @ 17:52:49',
+  version: 'v0.0.17',
 };
