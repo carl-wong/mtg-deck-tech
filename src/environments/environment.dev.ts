8443fb26abe130883a02d433f9781d742ffff9cb
Carl Wong <carl@winsoft.ca>
2020-01-10 02:29:42 -0500

    Version 0.0.14

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -6,6 +6,6 @@ export const environment = {
   // localApi: 'https://system.repvisor.com/decktech.api',
   oracleApi: 'https://decktech-api.narl.life',
   localApi: 'https://decktech-api.narl.life',
-  timestamp: 'Updated: 2020-01-09 @ 15:31:54',
+  timestamp: 'Updated: 2020-01-10 @ 02:23:07',
   version: 'DEV',
 };
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ export const environment = {
   defaultDecklist: '1 Plains\n1 Island\n1 Swamp\n1 Mountain\n1 Forest\n1 Wastes',
   oracleApi: 'https://decktech-api.narl.life',
   localApi: 'https://decktech-api.narl.life',
-  timestamp: 'Updated: 2020-01-09 @ 15:31:54',
-  version: 'v0.0.12',
+  timestamp: 'Updated: 2020-01-10 @ 02:23:07',
+  version: 'v0.0.14',
 };
