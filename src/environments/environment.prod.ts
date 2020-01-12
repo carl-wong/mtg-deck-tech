32ef72de356cbcd0c337c2151ad4239d74dd31e5
Carl Wong <carl@winsoft.ca>
2020-01-12 16:19:42 -0500

    Update environment.prod.ts
    For deployment on scry-x.com

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   defaultDecklist: '1 Plains\n1 Island\n1 Swamp\n1 Mountain\n1 Forest\n1 Wastes',
-  oracleApi: 'https://decktech-api.narl.life',
-  localApi: 'https://decktech-api.narl.life',
+  oracleApi: 'https://scry-x.com/decktech-api',
+  localApi: 'https://scry-x.com/decktech-api',
   timestamp: 'Updated: 2020-01-11 @ 10:11:56',
   version: 'v0.0.18',
 };
