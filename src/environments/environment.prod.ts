b774a0583a7f390b063b50e00fe7092a7e302ada
Carl Wong <carl@winsoft.ca>
2020-01-14 20:07:08 -0500

    Fixed upload script

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ export const environment = {
   defaultDecklist: '1 Plains\n1 Island\n1 Swamp\n1 Mountain\n1 Forest\n1 Wastes',
   baseHref: '/decktech/',
   apiUrl: 'https://scry-x.com/decktech-api',
-  timestamp: 'Updated: 2020-01-13 @ 11:18:59',
-  version: 'v0.0.21',
+  timestamp: 'Updated: 2020-01-14 @ 20:00:51',
+  version: 'v0.0.22',
 };
