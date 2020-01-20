a4aad53eb89d1a3231f18eea39d8d000d9ba1abe
Carl Wong <carl@winsoft.ca>
2020-01-20 11:42:11 -0500

    New version with tag optimization

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ export const environment = {
   defaultDecklist: '1 Plains\n1 Island\n1 Swamp\n1 Mountain\n1 Forest\n1 Wastes',
   baseHref: '/decktech/',
   apiUrl: 'https://scry-x.com/decktech-api',
-  timestamp: 'Updated: 2020-01-17 @ 23:36:55',
-  version: 'v0.0.28',
+  timestamp: 'Updated: 2020-01-20 @ 11:22:34',
+  version: 'v0.0.29',
 };
