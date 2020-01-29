ccb82bbda0bafd93ee6a1a5b8a0d0ab728abcfcc
Carl Wong <carl@winsoft.ca>
2020-01-29 00:09:01 -0500

    Version 1.0.4

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -4,6 +4,6 @@ export const environment = {
   defaultDecklist: '1 Alesha, Who Smiles at Death\n1 Altar of Dementia\n1 Animate Dead',
   baseHref: '/',
   apiUrl: 'https://scry-x.com/decktech-api',
-  timestamp: 'Updated: 2020-01-28 @ 23:36:28',
+  timestamp: 'Updated: 2020-01-29 @ 00:05:08',
   version: 'DEV',
 };
