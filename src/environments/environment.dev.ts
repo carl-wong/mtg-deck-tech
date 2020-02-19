2d0ecaa6a8acc3140e35dab3974db43356225e67
carl-wong <carl@winsoft.ca>
2020-02-19 12:46:21 -0500

    New version after upgrade to angular 9

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -4,6 +4,6 @@ export const environment = {
   defaultDecklist: '1 Alesha, Who Smiles at Death\n1 Altar of Dementia\n1 Animate Dead',
   baseHref: '/',
   apiUrl: 'https://scry-x.com/decktech-api',
-  timestamp: 'Updated: 2020-02-19 @ 09:53:38',
+  timestamp: 'Updated: 2020-02-19 @ 12:30:19',
   version: 'DEV',
 };
