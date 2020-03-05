756de6d01c89919dd882940a22d0d40a3e5460bc
carl-wong <carl@winsoft.ca>
2020-03-05 14:06:42 -0500

    New version for deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ export const environment = {
   defaultDecklist: '1 Plains\n1 Island\n1 Swamp\n1 Mountain\n1 Forest\n1 Wastes',
   baseHref: '/decktech/',
   apiUrl: 'https://scry-x.com/decktech-api',
-  timestamp: 'Updated: 2020-02-23 @ 23:59:42',
-  version: 'v1.0.14',
+  timestamp: 'Updated: 2020-03-05 @ 13:50:29',
+  version: 'v1.0.15',
 };
