f12166b7137779da8745a1a0adbbcd22f48cab52
carl-wong <carl@winsoft.ca>
2020-03-09 16:44:55 -0400

    New version with performance improvements

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,6 @@ export const environment = {
   defaultDecklist: '1 Alesha, Who Smiles at Death\n1 Altar of Dementia\n1 Animate Dead',
   baseHref: '/',
   apiUrl: 'http://localhost:3000',
-  timestamp: 'Updated: 2020-03-09 @ 14:07:09',
+  timestamp: 'Updated: 2020-03-09 @ 16:40:00',
   version: 'LOCAL',
 };
